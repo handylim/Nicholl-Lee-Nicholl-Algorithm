e721f53698f7782eea35043da9db97f2f176e844
Handy Lim <18360062+handylim@users.noreply.github.com>
2018-01-12 22:23:03 +0700

    Feature for line and dot are complete
    Can draw dots and lines on the canvas
    Can see the detail position of dots and lines on the canvas from the list
    Can delete the dots and lines from the canvas by clicking the delete button at the right of the list

--- index.js
+++ index.js
@@ -2,12 +2,113 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var canvasHeight = canvas.height;
 var canvasWidth = canvas.width;
+var data = []; // all the coordinate of the dots and lines will be stored here
+
+function addData (x1, y1, x2, y2) {
+	if (typeof x2 === 'undefined' && typeof y2 === 'undefined') // the data is a dot
+		data.push({
+			          type : 'dot',
+			          x    : x1,
+			          y    : y1
+		          });
+	else // the data is a line
+		data.push({
+			          type  : 'line',
+			          start : {
+				          x : x1,
+				          y : y1
+			          },
+			          end   : {
+				          x : x2,
+				          y : y2
+			          }
+		          });
+}
+
+function drawDot (x, y, color) {
+	color = (typeof color !== 'undefined') ? color : '#000'; // default parameter
+	context.beginPath();
+	context.arc(x, y, 2, 0, 2 * Math.PI);
+	// set dot color
+	context.fillStyle = color;
+	context.fill();
+}
+
+function drawLine (x1, y1, x2, y2, color) {
+	color = (typeof color !== 'undefined') ? color : '#000'; // default parameter
+	context.beginPath();
+	context.moveTo(x1, y1);
+	context.lineTo(x2, y2);
+	// set line color
+	context.lineWidth = 2;
+	context.strokeStyle = color;
+	context.stroke();
+}
+
+function clearCanvas () {
+	context.clearRect(0, 0, canvasWidth, canvasHeight);
+}
+
+function deleteData (x1, y1, x2, y2) { // x2 and y2 are undefined in deleting dot
+	var temp = JSON.parse(JSON.stringify(data)); // deep copy
+	clearCanvas();
+	for (var i = 0; i < temp.length; i++)
+		if (temp[i].type === 'dot')
+			(temp[i].x === x1 && temp[i].y === y1) ? data.splice(i, 1) : drawDot(temp[i].x, temp[i].y);
+		else if (temp[i].type === 'line')
+			(temp[i].start.x === x1 && temp[i].start.y === y1 && temp[i].end.x === x2 && temp[i].end.y === y2) ? data.splice(i, 1) : drawLine(temp[i].start.x, temp[i].start.y, temp[i].end.x, temp[i].end.y);
+}
 
 canvas = $('#canvas');
 context.transform(1, 0, 0, -1, 0, canvasHeight);
-var list = $('ul.collection.with-header');
+var mode        = 'line',
+    inProcess   = false,
+    dotCounter  = 0,
+    lineCounter = 0,
+    list = $('ul.collection.with-header');
 
 $(document).ready(function () {
 	$('h1').remove();
 	list.css('height', canvasHeight);
+	canvas.on('mousemove', function (event) {
+		var x = event.offsetX;
+		var y = canvasHeight - event.offsetY;
+		$('#coordinate').text('(' + x + ', ' + y + ')');
+	});
+	canvas.on('mouseout', function () {
+		$('#coordinate').text('(-, -)');
+	});
+	canvas.on('click', function (event) {
+		var x    = event.offsetX,
+		    y    = canvasHeight - event.offsetY;
+
+		switch (mode) {
+			case 'dot':
+				drawDot(x, y);
+				addData(x, y);
+				dotCounter++;
+				list.html(list.html() + '<li class="collection-item"><div>Dot ' + dotCounter + ' (' + x + ', ' + y + ')' + '<span class="secondary-content" onclick="deleteData(' + x + ', ' + y + '); $(this).parent().parent().remove();"><i class="material-icons">delete</i></span></div></li>');
+				break;
+			case 'line':
+				if (!inProcess) {
+					tempX = x;
+					tempY = y;
+					inProcess = true;
+				}
+				else {
+					drawLine(tempX, tempY, x, y);
+					addData(tempX, tempY, x, y);
+					inProcess = false;
+					lineCounter++;
+					list.html(list.html() + '<li class="collection-item"><div>Line ' + lineCounter + ' (' + tempX + ', ' + tempY + ') to (' + x + ', ' + y + ')' + '<span class="secondary-content" onclick="deleteData(' + tempX + ', ' + tempY + ', ' + x + ', ' + y + '); $(this).parent().parent().remove();"><i class="material-icons">delete</i></span></div></li>');
+				}
+				break;
+		}
+	});
+	$('#dotButton').on('click', function () {
+		mode = 'dot';
+	});
+	$('#lineButton').on('click', function () {
+		mode = 'line';
+	});
 });
